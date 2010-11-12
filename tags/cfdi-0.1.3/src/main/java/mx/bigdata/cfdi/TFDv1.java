051ded6c4de5d2d5eb9a1860fa175de65f672d8d
bigdata.mx@gmail.com <bigdata.mx@gmail.com@8412b91d-32a6-fd75-0f2b-73ba46573a7d>
2010-11-12 18:23:14 +0000

    [maven-release-plugin]  copy for tag cfdi-0.1.3

---
+++ tags/cfdi-0.1.3/src/main/java/mx/bigdata/cfdi/TFDv1.java
@@ -0,0 +1,33 @@
+/*
+ *  Copyright 2010 BigData.mx
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package mx.bigdata.cfdi;
+
+import java.security.cert.X509Certificate;
+
+import mx.bigdata.sat.cfdi.CFDv3;
+
+/**
+ * @deprecated Reemplazado por {@link mx.bigdata.sat.cfdi.TFDv1}
+ * a partir de la version 0.1.3
+ */
+@Deprecated public final class TFDv1 extends mx.bigdata.sat.cfdi.TFDv1 {
+
+  public TFDv1(CFDv3 cfd, X509Certificate cert) throws Exception {
+    super(cfd, cert);
+  }
+
+}
